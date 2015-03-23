aa28ee83edaf6187f4788ed56b1272b5a26f2f60
MarkJensen55 <markjensen@tds.net>
2015-03-23 14:18:20 -0400

    Do something!
    Output input pars to screen

--- MondayHello/Program.cs
+++ MondayHello/Program.cs
@@ -6,11 +6,18 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace MondayHello  // Defines the "root name" of the classes and functions found here
-{
+{ 
     class Program  // this inherits from class program
     {
         static void Main(string[] args) // the main function is where the program starts running
+                                        // command line parameters are input here
         {
+
+            Console.WriteLine("Hello " + args[0] + " " + args[1]);  // as input
+            Console.WriteLine("Hello " + args[1] + ", " + args[0]);  // in last,first order
+
+
+            Console.ReadLine();
         }
     }
 }
