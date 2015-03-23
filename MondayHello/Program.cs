8cfff13d884f3f4fec632918c1931da2a06836fc
MarkJensen55 <markjensen@tds.net>
2015-03-23 13:58:58 -0400

    Added Comments

--- MondayHello/Program.cs
+++ MondayHello/Program.cs
@@ -1,14 +1,15 @@
-﻿using System;
+﻿// Using statements tell the compiler where to find the class definitions needed
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MondayHello
+namespace MondayHello  // Defines the "root name" of the classes and functions found here
 {
-    class Program
+    class Program  // this inherits from class program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) // the main function is where the program starts running
         {
         }
     }
